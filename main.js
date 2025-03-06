983efb1269e8fe72949970675cf93bbf2ce4a286
Shishir707 <mirshr8@gmail.com>
2025-03-06 12:45:20 +0600

    show price as user input

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 var kg = parseFloat(prompt("What quantity do you want to buy??"))
-var total = kg + 500
+var total = 0;
+
+if (document.body.id === "meatPrice") {
+    total = kg *500;
+} 
+else if (document.body.id === "muttonPrice") {
+    total = kg *700;
+}
+else{
+    total = kg * 800;
+}
 
 console.log("Dear Sir/Mam You have to pay:", total);
-document.getElementById("showcontent").textContent = total;
+document.getElementById("priceM").innerText = total; 
